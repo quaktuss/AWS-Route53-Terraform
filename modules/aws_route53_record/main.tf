bc9c7a4a5c85e92df76c8c5f4315a7275d689dc2
qaktuss <mathieupheron1@gmail.com>
2023-12-31 12:54:09 +0100

    🚧 WIP : Add ACM's module
    🚨 Problem
    To hard to use acm resources directly
    
    💡 Solution
    ADD ACM module
    📝 Note

--- modules/aws_route53_record/main.tf
+++ modules/aws_route53_record/main.tf
@@ -3,5 +3,26 @@ resource "aws_route53_record" "www" {
   name    = "${var.record_name}.${data.aws_route53_zone.primary.name}"
   type    = var.type
   ttl     = 300
-  records = ["3.255.242.95"]
+  records = ["34.255.195.138"]
+}
+
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 4.0"
+
+  domain_name  = "${var.record_name}.${data.aws_route53_zone.primary.name}"
+  zone_id      = data.aws_route53_zone.primary.zone_id
+
+  validation_method = "DNS"
+
+# subject_alternative_names = [
+#   "*.my-domain.com",
+#   "app.sub.my-domain.com",
+# ]
+
+  wait_for_validation = true
+
+  tags = {
+    Name = "terraform-test.matih.eu"
+  }
 }
